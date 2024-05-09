687cd25ed1962c029722a5ccf2e9ad62775f3762
Kiril Volskiy <vtkkllx2367@gmail.com>
2024-05-09 21:35:02 +0300

    feat: PayMethods layout

--- src/components/HelpForm/components/FinancialHelpForm/FinancialHelpForm.jsx
+++ src/components/HelpForm/components/FinancialHelpForm/FinancialHelpForm.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './FinancialHelpForm.module.scss'
+import PayMethods from '../../../PayMethods/PayMethods'
 
 const FinancialHelpForm = () => {
-  return (
-    <div className={`${s.financialHelpForm} `} >
-        Financ
+  
+   const [payMethod, setPayMethod] = useState(null)
+
+   return (
+    <div className={s.financialHelpForm}>
+        <div className={s.payMethodsContainer}>
+           <p>Виберіть спосіб оплати</p>
+           <PayMethods/>
+        </div>
+        <div className={s.cardDataContainer}>
+           <p>Введіть наступні данні</p>
+        </div>
     </div>
   )
 }
