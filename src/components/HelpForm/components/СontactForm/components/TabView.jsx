fd4505a0feb6df9999b9d97136793a77d66d50cc
Kiril Volskiy <vtkkllx2367@gmail.com>
2024-05-10 17:44:30 +0300

    feat: tabs keyboard navigation

--- src/components/HelpForm/components/СontactForm/components/TabView.jsx
+++ src/components/HelpForm/components/СontactForm/components/TabView.jsx
@@ -4,23 +4,55 @@ import s from "./TabView.module.scss";
 const TabView = ({ tabs, tabButtons, active }) => {
   const [activeTab, setActiveTab] = useState(active);
 
+  const keyboardNavigation = (e, i) => {
+    const tab = e.target;
+    const key = e.key;
+
+    switch (key) {
+      case "ArrowLeft": {
+        const prev = tab.previousElementSibling 
+        const prevIndex = i - 1
+        if (prev && tabButtons[prevIndex]) {
+          prev.focus();
+          setActiveTab(tabButtons[prevIndex].name)
+        }
+
+        break
+      }
+      case "ArrowRight": {
+        const next = tab.nextElementSibling
+        const nextIndex = i + 1
+        if (next && tabButtons[nextIndex]) {
+          next.focus();
+          setActiveTab(tabButtons[nextIndex].name)
+        }
+        
+        break
+      }
+
+    }
+  };
 
   return (
     <div className={s.tabs}>
       <div className={s.tabButtons}>
-        {tabButtons.map((tab) => (
-          <button
-            key={tab.name}
-            className={
-              tab.name === active
-                ? `${s.tabButton} ${s.tabButtonActive}`
-                : s.tabButton
-            }
-            onClick={() => setActiveTab(tab.name)}
-          >
-            {tab.title}
-          </button>
-        ))}
+        {tabButtons.map((tab, i) => {
+          const isActive = tab.name === active;
+
+          return (
+            <button
+              tabIndex={isActive ? 0 : -1}
+              onKeyDown={(e) => keyboardNavigation(e, i)}
+              key={tab.name}
+              className={
+                isActive ? `${s.tabButton} ${s.tabButtonActive}` : s.tabButton
+              }
+              onClick={() => setActiveTab(tab.name)}
+            >
+              {tab.title}
+            </button>
+          );
+        })}
       </div>
       <div className={s.tabPanel}>{tabs[activeTab]?.body}</div>
     </div>
---
+++ src/ui/button/MainButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './MainButton.module.scss'
+
+const MainButton = () => {
+  return (
+    <div>MainButton</div>
+  )
+}
+
+export default MainButton
