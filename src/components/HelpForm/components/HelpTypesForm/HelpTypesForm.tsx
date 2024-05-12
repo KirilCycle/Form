1134b1b9e4fa4036af554b4be44873d44fc4fcc3
Kiril Volskiy <vtkkllx2367@gmail.com>
2024-05-12 21:28:43 +0300

    destructorization

--- src/components/HelpForm/components/HelpTypesForm/HelpTypesForm.tsx
+++ src/components/HelpForm/components/HelpTypesForm/HelpTypesForm.tsx
@@ -1,19 +1,16 @@
 import React, { useState, useRef, useEffect } from "react";
 import s from "./HelpTypesForm.module.scss";
 import FinancialHelpForm from "./forms/FinancialHelpForm/FinancialHelpForm";
-import HelpBodyWrapper from "./components/HelpBodyWrapper/HelpBodyWrapper";
-import PointerSlot from "../../../PointerSlot/PointerSlot";
-import { keyboardNavigation } from "../../../../utils/accessebility";
 import WalletIcon from "../../../../assets/icons/svg/WalletIcon";
-import HelpTypesButtonsList from "./components/HelpTypesButtonsList/HelpTypesButtonList";
 import HandIcon from "../../../../assets/icons/svg/HandIcon";
 import TshirtIcon from "../../../../assets/icons/svg/TshirtIcon";
 import HeartIcon from "../../../../assets/icons/svg/HeartIcon";
+import HelpTypesTabs from "./components/HelpTypesTabs/HelpTypesTabs";
 
 const HelpTypesForm = ({ formRef, errorMessages, setPayMethod, payMethod }) => {
   const [activeHelpType, setActiveTypeHelp] = useState("financial");
 
-  const helpTypes = [
+  const helpTypesTabs = [
     {
       type: "action",
       title: "Зробити",
@@ -51,15 +48,7 @@ const HelpTypesForm = ({ formRef, errorMessages, setPayMethod, payMethod }) => {
     <div className={s.helpContainer}>
       <h2>Види допомоги</h2>
       <p className={s.helpDescritption}>Ви можете змінити вид допомоги</p>
-      <HelpTypesButtonsList
-        setActiveTypeHelp={setActiveTypeHelp}
-        activeHelpType={activeHelpType}
-        helpTypes={helpTypes}
-      />
-      <HelpBodyWrapper
-        activeTab={activeHelpType}
-        tabPanels={helpTypes}
-      ></HelpBodyWrapper>
+      <HelpTypesTabs tabs={helpTypesTabs} activeType={activeHelpType} setActiveType={setActiveTypeHelp} />
     </div>
   );
 };
---
+++ src/components/HelpForm/components/HelpTypesForm/components/HelpTypesTabs/HelpTypesTabs.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import s from "./HelpTypesTabs.module.scss";
+import PointerSlot from "../../../../../PointerSlot/PointerSlot";
+import { keyboardNavigation } from "../../../../../../utils/accessebility";
+import { classNames } from "../../../../../../utils/styling";
+
+const HelpTypesTabs = ({ tabs, activeType, setActiveType }) => {
+  return (
+    <>
+      <div role="tablist" className={s.helpTypesList}>
+        {tabs.map(({ type, title, icon }, i) => {
+          const isActive = type === activeType;
+          return (
+            <div
+              tabIndex={isActive ? 0 : -1}
+              onKeyDown={(e) =>
+                keyboardNavigation(e, i, tabs, "type", setActiveType)
+              }
+              className={s.helpType}
+              key={type}
+            >
+              <PointerSlot isActive={isActive}>
+                <button
+                  type="button"
+                  role="tab"
+                  id={`tab-${type}`}
+                  aria-label={`select tab ${type}`}
+                  aria-controls={`tabpanel-${type}`}
+                  aria-selected={isActive}
+                  tabIndex={-1}
+                  onClick={() => setActiveType(type)}
+                  className={classNames(s.helpListBtn, isActive && s.activeBtn)}
+                >
+                  {icon ? icon : null}
+                </button>
+              </PointerSlot>
+              <p>{title}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className={s.helpBodyWrappper}>
+        {tabs.map(({ panel, type }) => {
+          const isActive = type === activeType;
+
+          return (
+            <div
+              role="tabpanel"
+              aria-labelledby={`tab-${type}`}
+              id={`tabpanel-${type}`}
+              tabIndex="0"
+              key={type}
+              style={{ display: `${isActive ? "block" : "none"}` }}
+            >
+              {panel}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default HelpTypesTabs;
